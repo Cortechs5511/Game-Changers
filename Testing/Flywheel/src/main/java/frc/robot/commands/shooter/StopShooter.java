2ec59ca04f419eb2de16d93178866899ec75cb67
ericliu0000 <55063842+ericliu0000@users.noreply.github.com>
2021-02-24 20:27:20 -0500

    Fix stop shooter command
    Adjusted stop shooter command to fix flywheel hangs when using the command

--- Testing/Flywheel/src/main/java/frc/robot/commands/shooter/StopShooter.java
+++ Testing/Flywheel/src/main/java/frc/robot/commands/shooter/StopShooter.java
@@ -33,6 +33,9 @@ public class StopShooter extends CommandBase {
         m_feeder.setFeeder2Speed(0);
         m_feeder.setFeeder3Speed(0);
 
+        m_shooter.setPIDReference(0);
+        m_shooter.setSpeed(0);
+
         m_shooter.setRampRate(1.5);
 
         m_drive.setLeft(0);
@@ -51,9 +54,6 @@ public class StopShooter extends CommandBase {
 
     @Override
     public void end(boolean interrupted) {
-        m_shooter.setPIDReference(0);
-        m_shooter.setSpeed(0);
-
         m_oi.setLeftRumble(0);
         m_oi.setRightRumble(0);
     }
